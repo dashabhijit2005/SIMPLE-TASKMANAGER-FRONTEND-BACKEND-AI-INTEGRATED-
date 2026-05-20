e4b4e05fe1c15ca0692ceae24290e1bdec3d57c3
Abhijit Dash <dashabhijeet650@gmail.com>
2026-05-20 19:52:51 +0530

    Add files via upload

--- task manager backend/middleware/authMiddleware.js
+++ task manager backend/middleware/authMiddleware.js
@@ -1,1 +1,26 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
+const authMiddleware = (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        message: "No Token Found",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    res.status(401).json({
+      message: "Invalid Token",
+    });
+  }
+};
+
+module.exports = authMiddleware;
