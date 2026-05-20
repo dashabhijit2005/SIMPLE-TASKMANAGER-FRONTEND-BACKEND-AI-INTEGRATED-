5dcc9bebc7318e3248c4b5b004957daf4cedfb98
Abhijit Dash <dashabhijeet650@gmail.com>
2026-05-20 19:50:23 +0530

    Add files via upload

--- task manager backend/config/db.js
+++ task manager backend/config/db.js
@@ -1,1 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.log("Database Error:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
