d2c2c32cdea8649fdc2c6a350dc82e2b920b9ff8
Abhijit Dash <dashabhijeet650@gmail.com>
2026-05-20 19:53:53 +0530

    Add files via upload

---
+++ task manager backend/models/Task.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+    },
+
+    category: {
+      type: String,
+      default: "General",
+    },
+
+    priority: {
+      type: String,
+      default: "Medium",
+    },
+
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Task", taskSchema);
